0dcd0f3067b637b127be73ed0f499029cb479778
Rename52 <138089364+Rename52@users.noreply.github.com>
2024-05-09 19:41:05 +0900

    [ADD] Unauthorized_Access

---
+++ web/Unauthorized_Access/deploy/app.py
@@ -0,0 +1,149 @@
+from flask import Flask, render_template, request, redirect, url_for, session
+import sqlite3
+import csv
+
+app = Flask(__name__)
+app.secret_key = 'FAKE_SECRET'  
+
+def init_db():
+    conn = sqlite3.connect('users.db')
+    cursor = conn.cursor()
+    
+    cursor.execute('''
+        DROP TABLE users;
+    ''')
+    
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS users (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            rank INTEGER NOT NULL,
+            username TEXT UNIQUE NOT NULL,
+            password TEXT NOT NULL
+        );
+    ''')
+    # 회원가입 폼이 구현되지않아 임시계정 id:guest/pw:guest를 생성함 관리자를 위한 admin 계정을 개설했으니 이메일을 확인바람.
+    txt_file_path = 'users.txt'
+
+    users = []
+    with open(txt_file_path, 'r') as txtfile:
+        for line in txtfile:
+            rank, username, password = line.strip().split(',')
+            users.append((rank, username, password))
+
+    cursor.executemany('INSERT INTO users (rank, username, password) VALUES (?, ?, ?)', users)
+
+    conn.commit()
+    conn.close()
+
+init_db()
+
+@app.route('/')
+def index():
+    if 'username' in session:
+        username = session['username']
+        password = session['password']
+        rank = session['rank']
+        return render_template('loginindex.html', username=username, rank=rank)
+    else:
+        return render_template('index.html')
+
+@app.route('/logout', methods=['POST'])
+def logout():
+    session.pop('username', None)
+    session.pop('password', None)
+    session.pop('rank', None)
+    return '''
+    <script>
+        alert("로그아웃 성공!");
+        window.location.href = "/";
+    </script>
+    '''
+
+@app.route('/login', methods=['POST'])
+def login():
+    username = request.form.get('username')
+    password = request.form.get('password')
+    
+    conn = sqlite3.connect('users.db')
+    cursor = conn.cursor()
+    query = "SELECT rank, password FROM users WHERE username = ?"
+    cursor.execute(query, (username,))
+    result = cursor.fetchone()
+    
+    conn.close()
+
+    if result is not None:
+        rank, db_password = result
+        if db_password == password:
+            session['username'] = username
+            session['password'] = password
+            session['rank'] = rank
+            return '''
+            <script>
+                alert("로그인 성공!");
+                window.location.href = "/";
+            </script>
+            '''
+    return '''
+    <script>
+        alert("로그인 실패!");
+        window.location.href = "/";
+    </script>
+    '''
+
+@app.route('/guest', methods=['POST'])
+def guest_login():
+    session['username'] = "guest"
+    session['password'] = "guest"
+    session['rank'] = 0
+    return '''
+    <script>
+        alert("게스트 로그인 성공!");
+        window.location.href = "/";
+    </script>
+    '''
+
+def escape_string(s):
+    forbidden_words = ["update", "drop", "delete", "create", "insert"]
+    s_lower = s.lower()
+    for word in forbidden_words:
+        s_lower = s_lower.replace(word, "")
+    return s_lower
+
+@app.route('/register', methods=['POST'])
+def register():
+    username = request.form.get('username')
+    password = request.form.get('password')
+
+    escaped_username = escape_string(username)
+    escaped_password = escape_string(password)
+    
+    conn = sqlite3.connect('users.db')
+    cursor = conn.cursor()
+    
+    try:
+        query = f'INSERT INTO users (rank, username, password) VALUES (0,"{escaped_username}", "{escaped_password}")'
+        cursor.execute(query)
+        conn.commit()
+        
+        query = "SELECT COUNT(*) FROM users WHERE rank >= 1"
+        cursor.execute(query)
+        total = cursor.fetchone()[0]
+        conn.commit()
+        
+        if total >= 1:
+            query = "DELETE FROM users WHERE username != 'guest'"
+            cursor.execute(query)
+            conn.commit()
+            conn.close()
+            with open('flag.txt', 'r') as f:
+                flag_content = f.read().strip()
+            return f'성공하셨군요..? Flag: {flag_content}'
+        return '회원가입이 완료되었습니다.'
+        conn.close()
+    except sqlite3.IntegrityError:
+        conn.close()
+        return '이미 존재하는 사용자입니다.'
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000,debug=False)
