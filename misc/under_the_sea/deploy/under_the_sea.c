35584d4d4e3989a329280636a1eea5e413abcbfd
orol7894 <aqsed147@gmail.com>
2024-05-13 15:31:04 +0900

    pwn/misc bug fixed

--- misc/under_the_sea/deploy/under_the_sea.c
+++ misc/under_the_sea/deploy/under_the_sea.c
@@ -5,13 +5,15 @@ int main() {
     int value;
     
     scanf("%d",&value);
-    if (value < 0)
+    if (value < 0){
         value-=1004;
-    if (value > 0) {
-        system("/bin/sh");
-    } else {
-        printf("Value: %d\n", value);
+        if (value > 0) {
+            system("/bin/sh");
+        } else {
+            printf("Value: %d\n", value);
+        }
     }
+        
 
     return 0;
 }
