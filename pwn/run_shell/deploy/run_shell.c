f471b7370cb7d2060289efba6a99e0d1bd18b83d
orol7894 <orol7894@gmail.com>
2024-05-17 19:44:41 +0900

    [ADD] run_shell Time out

--- pwn/run_shell/deploy/run_shell.c
+++ pwn/run_shell/deploy/run_shell.c
@@ -1,12 +1,19 @@
 //gcc -m32 -fno-stack-protector -no-pie -o run_shell run_shell.c
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include <signal.h>
+#include <unistd.h>
+
+void timeout_handler(int signum) {
+    printf("\nTimeout occurred. Exiting program.\n");
+    exit(1);
+}
 
 void print_string() {
     char buffer[256];
     setbuf(stdout, NULL);
     setbuf(stdin, NULL);
+    signal(SIGALRM, timeout_handler);
+    alarm(30);
 
     printf("stdout = %p\n", stdout);
     printf("Enter a string: \n");
